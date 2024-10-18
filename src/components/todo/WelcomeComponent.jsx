43b79b4c8e7aa47f3475e59540735f29d460c2d5
Aragajorge <3glasser@gmail.com>
2024-10-17 22:57:59 -0300

    Connected Spring Boot REST API with React Frontend

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,8 +1,35 @@
 import {useParams, Link} from 'react-router-dom'
-
+import { useState } from 'react'
+import { retrieveHelloWorldBean, retrieveHelloWorldPathVariable} from './api/HelloWorldApiService'
 export default function WelcomeComponent() {
 
     const {username} = useParams()
+
+    const [message, setMessage] = useState(null)
+
+    function callHelloWorldRestApi() {
+
+        // console.log("Hello World!")
+
+        // retrieveHelloWorldBean()
+        // .then((response) => successfulResponse(response))
+        // .catch((error) => errorResponse(error))
+        // .finally(() => console.log('cleanup'))
+
+        retrieveHelloWorldPathVariable('Jorge')
+        .then((response) => successfulResponse(response))
+        .catch((error) => errorResponse(error))
+        .finally(() => console.log('cleanup'))
+
+    }
+
+    function successfulResponse(response) {
+        console.log(response)
+        setMessage(response.data.message)
+    }
+    function errorResponse(error) {
+        console.log(error)
+    }
 
     return (
         <div className="WelcomeComponent">
@@ -10,6 +37,10 @@ export default function WelcomeComponent() {
             <div>
                 Manage your todos - <Link to='/todos'>Go here</Link>
             </div>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestApi }>Callo Hello World REST API</button>
+            </div>
+            <div className="text-info">{message}</div>
         </div>
     )
 }
